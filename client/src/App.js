caceaa41685b2086a915c0599bf98ba2e2bc0422
Dwalton55 <deonte.e.walton@gmail.com>
2018-07-17 13:37:16 -0400

    Created potential routes/ test branch

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './components/HomePage';
 import { Button, Icon } from 'semantic-ui-react'
 import FootNav from './components/FootNav';
 import DesktopNav from './components/DeskTopNav';
+import IndividualCity from './components/IndividualCity';
 
 const AppBody = styled.div`
   text-align: center;
@@ -45,6 +46,8 @@ class App extends Component {
           <Title>Vagabond</Title>
           <Switch>
             <Route exact path='/' component={HomePage}/>
+            <Route exact path='/:city' component={IndividualCity}/>
+            <Route exact path='/:city/post' component={IndividualCity}/>
           </Switch>
           <FootNav/>
         </AppBody>
